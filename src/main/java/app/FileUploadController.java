a1e9f75267058f936a2af862bceff6b0feb87444
deniandriancode <deniandriancode@gmail.com>
2023-07-07 18:26:11 +0700

    init

---
+++ src/main/java/app/FileUploadController.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.io.*;
+import java.util.stream.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.core.io.*;
+import org.springframework.http.*;
+import org.springframework.ui.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+}
